53bf3089137a31e7de13af691f983ca8e42797b5
Maruko1337 <quyf0516@outlook.com>
2025-01-09 23:24:58 -0500

    first commit

---
+++ set_up.sh
@@ -0,0 +1,6 @@
+sudo apt update
+# password: Qyhgeo$76611
+sudo apt install git
+git config --global user.name "Maruko1337"
+git config --global user.email "quyf0516@outlook.com"
+git clone " https://github.com/Maruko1337/tree_delineation.git"
